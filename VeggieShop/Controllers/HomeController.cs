281bfb8f403041b041f0be015f00fca10450cdab
pjbazin <pascal.bazin@gemalto.com>
2017-02-24 14:04:53 +0100

    Create VeggieShop

---
+++ VeggieShop/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Reflection;
+
+namespace gemalto.wpwg.pads.Controllers
+{
+  public class HomeController : Controller
+  {
+    static Assembly EntryAssembly;
+    static string AssemblyView;
+
+    public HomeController()
+    {
+      EntryAssembly = Assembly.GetEntryAssembly();
+      AssemblyView = $"2017 - {EntryAssembly.GetName().Name} - v{EntryAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion}";
+    }
+
+
+    public IActionResult Index()
+    {
+      ViewData["Assembly"] = AssemblyView;
+      return View();
+    }
+
+
+    public IActionResult About()
+    {
+      ViewData["Assembly"] = AssemblyView;
+      return View();
+    }
+
+
+    public IActionResult Registrations()
+    {
+      ViewData["Assembly"] = AssemblyView;
+      return View();
+    }
+
+
+    public IActionResult Contact()
+    {
+      ViewData["Assembly"] = AssemblyView;
+      return View();
+    }
+
+
+    public IActionResult Error()
+    {
+      ViewData["Assembly"] = AssemblyView;
+      return View();
+    }
+  }
+}
---
+++ VeggieShop/wwwroot/js/site.js
@@ -0,0 +1,37 @@
+﻿// Site Reference
+var _version = "0.8.0221D"
+
+// Liste of basic items
+var items = {
+    carrots: {
+        name: "carrots",
+        src: "~/images/carrot jpg",
+        price: 0.01,
+    },
+    kales: {
+        name: "kales",
+        price: 0.02,
+    },
+    potatoes: {
+        name: "potatoes",
+        price: 0.10,
+    },
+    tomatoes: {
+        name: "tomatoes",
+        price: 1,
+    },
+}
+
+
+// Payment structure to be used by the payment App request
+var order = {
+    ref: "",
+    currency: "EUR",
+    price: 0,
+    tax: 0,
+    shipping: 0,
+    total: 0
+}
+
+
+
